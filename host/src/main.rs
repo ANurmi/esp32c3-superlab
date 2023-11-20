622e3172fa8e794cee18a9664c305a87bf1eb6da
Tom Szymkowiak <ziggys_42@hotmail.com>
2023-11-20 10:50:13 +0200

    implement command timeout detection + recovery

--- host/src/main.rs
+++ host/src/main.rs
@@ -10,7 +10,7 @@
 //!
 
 // Rust dependencies
-use std::{io::Read, mem::size_of};
+use std::{io::{Read, ErrorKind}, mem::size_of, time::Duration};
 
 // Libraries
 use corncobs::{max_encoded_len, ZERO};
@@ -20,6 +20,8 @@ use serial2::SerialPort;
 use host::open;
 use shared::{deserialize_crc_cobs, serialize_crc_cobs, Command, Message, Response}; // local library
 
+const CMD_TIMEOUT_SECS : Duration = Duration::from_secs(2); 
+
 const IN_SIZE: usize = max_encoded_len(size_of::<Response>() + size_of::<u32>());
 const OUT_SIZE: usize = max_encoded_len(size_of::<Command>() + size_of::<u32>());
 
@@ -27,20 +29,26 @@ type InBuf = [u8; IN_SIZE];
 type OutBuf = [u8; OUT_SIZE];
 
 fn main() -> Result<(), std::io::Error> {
+
+    println!("\n\nRTIC2 - Reliable Serial Communication: Host Application\n");
+
     let mut port = open()?;
+
+    port.set_read_timeout(CMD_TIMEOUT_SECS)?;
+    println!("Command timeout set to {:?} second(s).\n", port.get_read_timeout().unwrap().as_secs());
 
     let mut out_buf = [0u8; OUT_SIZE];
     let mut in_buf = [0u8; IN_SIZE];
 
     let cmd = Command::Set(0x12, Message::B(12), 0b001);
-    println!("request {:?}", cmd);
+    println!("--> Request: {:?}\n", cmd);
     let response = request(&cmd, &mut port, &mut out_buf, &mut in_buf)?;
-    println!("response {:?}", response);
+    println!("<-- Response: {:?}\n", response);
 
     let cmd = Command::Get(0x12, 12, 0b001);
-    println!("request {:?}", cmd);
+    println!("--> Request: {:?}\n", cmd);
     let response = request(&cmd, &mut port, &mut out_buf, &mut in_buf)?;
-    println!("response {:?}", response);
+    println!("<-- Response: {:?}\n", response);
     Ok(())
 }
 
@@ -50,24 +58,52 @@ fn request(
     out_buf: &mut OutBuf,
     in_buf: &mut InBuf,
 ) -> Result<Response, std::io::Error> {
-    println!("out_buf {}", out_buf.len());
+    
     let to_write = serialize_crc_cobs(cmd, out_buf);
-    port.write_all(to_write)?;
+    let mut tx_complete : bool = false;
 
-    println!("{:?}", to_write);
+    while tx_complete == false {
 
-    let mut index: usize = 0;
-    loop {
-        let slice = &mut in_buf[index..index + 1];
-        if index < IN_SIZE {
-            index += 1;
-        }
-        port.read_exact(slice)?;
-        if slice[0] == ZERO {
-            println!("-- cobs package received --");
-            break;
+        port.write_all(to_write)?;
+
+        println!("Request written... Awaiting response.\n");
+
+        let mut index: usize = 0;
+        
+        loop {
+            
+            let slice = &mut in_buf[index..index + 1];
+            
+            if index < IN_SIZE {
+                index += 1;
+            }
+
+            match port.read_exact(slice) {
+                Ok(_) => {
+                    // do nothing
+                },
+                // check for timeout and re-send packet if detected
+                Err(e) => {
+                    match e.kind() {
+                        ErrorKind::TimedOut => {
+                            println!("[Error] - Request time-out expired!\n");
+                            break;
+                        },
+                        _ => {
+                            println!("[Error] - There was a problem reading a byte from the buffer: {:?}\n", e);
+                            return Err(e);
+                        },
+                    };
+                },
+            };
+
+            if slice[0] == ZERO {
+                println!("Response received!\n");
+                tx_complete = true;
+                break;
+            }
         }
     }
-    println!("cobs index {}", index);
+
     Ok(deserialize_crc_cobs(in_buf).unwrap())
 }
